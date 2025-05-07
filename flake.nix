319773d1dc28fdffc58b0844791ccf10dc641a44
kotudemo <130310308+kotudemo@users.noreply.github.com>
2025-05-07 21:05:14 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,14 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ] (system: function nixpkgs.legacyPackages.${system});
+
+  outputs = { self, nixpkgs, ... }@inputs: let
+    forAllSystems = f: nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system: f (import nixpkgs { inherit system; }).legacyPackages.${system});
   in {
     formatter = forAllSystems (pkgs: pkgs.alejandra);
     packages = forAllSystems (pkgs: {
